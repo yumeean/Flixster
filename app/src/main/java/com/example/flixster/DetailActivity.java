af118f766ba96dc5a6d23b66d218e3eb040125a3
yumeean <ig62@njit.edu>
2021-09-24 21:38:32 -0400

    Hid YouTube API Key

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -23,7 +23,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyDdGyYKSq7YX8m3GEtky6cb71fVA-EClck";
+    public static final String YOUTUBE_API_KEY = BuildConfig.YT_KEY;
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
